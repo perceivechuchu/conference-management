7b976831b8b384d81862123fbb8f4e6553590449
root <root@esolutions.site>
2017-04-12 22:20:54 +0200

    First commit

--- src/com/mycompany/conferencemanagement/util/Constant.java
+++ src/com/mycompany/conferencemanagement/util/Constant.java
@@ -4,6 +4,10 @@ public class Constant {
 
 	public static final String INPUT_FILE = "/home/perceive/Work/Interview/documents/DVT/ASIGNMENTS/input_file";
 	
+	public static final int MORNING_SESSION_START_TIME = 9 * 60;
+    public static final int LUNCH_SESSION_START_TIME = 12 * 60;
+    public static final int AFTERNOON_SESSION_START_TIME = 13 * 60;
+	
 	public static final String MORNING_SESSION = "Morning Session";
 	public static final String LUNCH = "Lunch";
 	public static final String AFTERNOON_SESSION = "Afternoon Session";
@@ -12,10 +16,6 @@ public class Constant {
 	public static final int MORNING_SESSION_DURATION = 180;
 	public static final int LUNCH_SESSION_DURATION = 60;
 	public static final int AFTERNOON_SESSION_DURATION = 240;
-	
-	public static final int MORNING_SESSION_START_TIME = 9 * 60;
-    public static final int LUNCH_SESSION_START_TIME = 12 * 60;
-    public static final int AFTERNOON_SESSION_START_TIME = LUNCH_SESSION_START_TIME + LUNCH_SESSION_DURATION;
     
     public static final int LIGHTING_PERIOD = 5;
 
--- src/com/mycompany/conferencemanagement/util/TimeFormatter.java
+++ src/com/mycompany/conferencemanagement/util/TimeFormatter.java
@@ -38,6 +38,7 @@ public class TimeFormatter {
 		} else {
 			formattedTime = " PM";
 		}
+		
 		formattedTime = formattedHours + ":" + formattedMinutes + formattedTime;
 
 		return formattedTime;
